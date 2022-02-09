79f34d69c2652ab6880ee38e95d40d03cf6153eb
Ridwan Ajibola <51078424+rajibola@users.noreply.github.com>
2022-02-09 17:46:06 +0100

    feat: Commented my function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,9 +1,23 @@
+/**
+ *
+ * @param {array} prices
+ * @param {string} currency
+ * @param {boolean} isPrice
+ * @returns {string} amount filtered
+ */
+
 export const filterPrice = (prices, currency, isPrice) => {
   const filtered = prices.find((price) => price.currency.symbol === currency);
   if (isPrice) return filtered?.amount;
   return currency + filtered?.amount;
 };
 
+/**
+ *
+ * @param {object} item
+ * @param {object} variants
+ * @returns {object} A new item object with coded id
+ */
 export let normarlize = (item, variants) => {
   let { prices, name, brand, gallery, id } = item;
   let cartId = id + "%" + JSON.stringify(variants);
@@ -17,8 +31,21 @@ export let normarlize = (item, variants) => {
   };
 };
 
+/**
+ *
+ * @param {string} id
+ * @returns {object} decoded variants objects coded in the id param
+ */
+
 export let getVariants = (id) =>
   JSON.parse(id.split("%").filter((_, idx) => idx)[0]);
+
+/**
+ *
+ * @param {array} cartItems
+ * @param {string} currency
+ * @returns {number} computed total price of the cart items
+ */
 
 export const getCartTotal = (cartItems, currency) =>
   cartItems
